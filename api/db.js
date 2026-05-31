6ae644be17461052a30e6e82f262820f829129d6
Luís Felipe Silva <felipepereiracript@gmail.com>
2026-05-31 12:49:27 -0300

    refactor: ajusta estrutura para deploy do json server na vercel

--- api/db.js
+++ api/db.js
@@ -1,9 +1,15 @@
+const fs = require('fs');
+const path = require('path');
 const jsonServer = require('json-server');
 const server = jsonServer.create();
-const router = jsonServer.router('db.json');
 const middlewares = jsonServer.defaults();
 
+const file = path.join(__dirname, '..', 'db.json');
+const data = JSON.parse(fs.readFileSync(file, 'utf8'));
+const router = jsonServer.router(data);
+
 server.use(middlewares);
+server.use(jsonServer.bodyParser);
 server.use(router);
 
 module.exports = server;
